59110ab98ad9071f6247908249b687e6fa4701cd
Azer Bulbul <sharkhack@gmail.com>
2018-02-06 15:01:20 -0800

    Create splitter.sh
    split strings in file than write first item to another file

---
+++ splitter.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#split strings in file than write first item to another file
+#in items.txt like;
+#item1 dsc ...n
+#item2 dsc ...n
+#item3 dsc ...n
+#....
+#itemN dcs ..n
+#
+#out - output.txt
+#item1
+#item2
+#....
+#itemN
+
+while IFS=$' ' read -r -a myArray
+do
+ echo "${myArray[0]}" >> "output.txt"
+done < "items.txt"
