50fcbb498365756b781d8795b1462a340d49db5a
Azer Bulbul <sharkhack@gmail.com>
2018-02-06 16:07:24 -0800

    Update splitter.sh

--- splitter.sh
+++ splitter.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
-
-#split strings in file than write first item to another file
-#in items.txt like;
-#item1 dsc ...n
-#item2 dsc ...n
-#item3 dsc ...n
-#....
-#itemN dcs ..n
 #
-#out - output.txt
-#item1
-#item2
-#....
-#itemN
+# Azer Bulbul
+#
+# split strings in file than write first item to another file
+# in items.txt like;
+# item1 dsc ...n
+# item2 dsc ...n
+# item3 dsc ...n
+# ....
+# itemN dcs ..n
+#
+# out - output.txt
+# item1
+# item2
+# ....
+# itemN
 
 while IFS=$' ' read -r -a myArray
 do
